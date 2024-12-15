f02b24e8d53b4c181d73f827e592b16791d20a7a
Dmitriy <unitofpeople@gmail.com>
2024-12-15 22:41:22 +0300

    bug fix autocomplete

--- src/app/directives/resize.directive.ts
+++ src/app/directives/resize.directive.ts
@@ -19,6 +19,7 @@ export class ResizeDirective {
       this.resizing = true;
     }
   }
+
   @HostListener('document:mouseup', ['$event'])
   onMouseUp(event: MouseEvent) {
     if (this.resizing) {
